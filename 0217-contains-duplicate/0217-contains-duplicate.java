b703943dda033e21e94bdd029c42a3877b1d453c
Youssef Khedr <youssefkhedr.2067@gmail.com>
2026-09-03 18:24:47 +0300

    Time: 13 ms (98.89%), Space: 108.8 MB (19.16%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        Set<Integer> set = new HashSet<>();
+
+        for (int num : nums) {
+            if (!set.add(num)) {
+                return true;
+            }
+        }
+        
+        return false;
+    }
+}
