8a22d6240d0a8211a778f4b72d47f602930449aa
Youssef Khedr <youssefkhedr.2067@gmail.com>
2026-09-03 20:25:53 +0300

    Time: 3 ms (94.04%), Space: 46.5 MB (34.28%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -0,0 +1,19 @@
+import java.util.Arrays;
+
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if(s.length()!=t.length()) return false;
+        
+        Set<Integer> set = new HashSet<>();
+        char[] sArray = s.toCharArray();
+        char[] tArray = t.toCharArray();
+
+        Arrays.sort(sArray);
+        Arrays.sort(tArray);
+
+        if(Arrays.equals(sArray,tArray)) 
+            return true;
+        else
+            return false;
+    }
+}
