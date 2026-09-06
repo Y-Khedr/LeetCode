06c6cffc3436d6f804091acb9d96b0a8d01b2ffd
Youssef Khedr <youssefkhedr.2067@gmail.com>
2026-09-06 20:28:37 +0300

    Time: 5 ms (62.9%), Space: 44.5 MB (59.81%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,16 +1,19 @@
+import java.util.*;
+
 class Solution {
     public boolean isAnagram(String s, String t) {
         if(s.length()!=t.length()) return false;
 
-        int[] char_count = new int[26];
+        int[] count = new int[26];
         int n = s.length();
-        for(int i=0; i<n; i++){
-            char_count[s.charAt(i) - 'a']++;
-            char_count[t.charAt(i) - 'a']--;
 
+        for(int i=0; i<n; i++){
+            count[s.charAt(i) - 'a']++;
+            count[t.charAt(i) - 'a']--;
         }
-        for(int count: char_count)
-            if(count!=0)
+
+        for(int i=0; i<26 ;i++)
+            if(count[i]!=0)
                 return false;
         return true;
     }
