f6974d956717a66b089b29c94f1e57f8ddcbefca
Youssef Khedr <youssefkhedr.2067@gmail.com>
2026-09-16 21:42:28 +0300

    Time: 0 ms (100%), Space: 46.8 MB (33.7%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.java
+++ 0141-linked-list-cycle/0141-linked-list-cycle.java
@@ -12,17 +12,15 @@ import java.util.*;
  */
 public class Solution {
     public boolean hasCycle(ListNode head) {
-        Set<ListNode> set = new HashSet<>();
-        ListNode curr = head;
+        ListNode slow = head;
+        ListNode fast = head;
 
-        while(curr != null){
-            if(set.contains(curr))
+        while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+            if(slow == fast)
                 return true;
-
-            set.add(curr);
-            curr = curr.next;
         }
-    
-    return false;
+        return false;
     }
 }
