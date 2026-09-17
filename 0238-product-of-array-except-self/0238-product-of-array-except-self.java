fdd7587a6552ae59b6b0132c6a13625cf76341ed
Youssef Khedr <youssefkhedr.2067@gmail.com>
2026-09-17 19:21:18 +0300

    Time: 2 ms (92.93%), Space: 64.3 MB (93.86%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.java
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.java
@@ -3,25 +3,22 @@ import java.util.*;
 class Solution {
     public int[] productExceptSelf(int[] nums) {
         int n = nums.length;
-
         int[] res = new int[n];
         int[] suffix = new int[n];
-        int sum = 1;
 
         res[0] = 1;
-        for(int i=1; i<n;i++)
-            res[i] = res[i-1] * nums[i-1];  
-
+        for(int i=1; i<n; i++)
+            res[i] = res[i-1] * nums[i-1];
+        
+        //n-1 indicated the last position, therefore n-2 is needed to access the position before last
         suffix[n-1] = 1;
-        for(int i = n-1 -1; i>=0; i--)
+        for(int i = n-2; i>=0; i--)
             suffix[i] = suffix[i+1] * nums[i+1];
 
         for(int i=0; i<n; i++)
-            res[i]*= suffix[i];
-
+            res[i]*=suffix[i];
+        
+        
         return res;
-
     }
-
-} 
-
+}
