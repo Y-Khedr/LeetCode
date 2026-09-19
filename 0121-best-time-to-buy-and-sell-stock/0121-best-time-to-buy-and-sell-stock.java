aa930c2edffed779ac54d807fc0c58d4008bdf0c
Youssef Khedr <youssefkhedr.2067@gmail.com>
2026-09-19 21:55:28 +0300

    Time: 2 ms (45.56%), Space: 94.4 MB (70.87%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
@@ -3,14 +3,15 @@ import java.util.*;
 class Solution {
     public int maxProfit(int[] prices) {
         int n = prices.length;
-        int max = 0;
 
         int left = 0;
         int right = 1;
-        while(right<n){
+        int max = 0;
+        while(right < n){
             int profit = prices[right] - prices[left];
-            if(prices[left]<prices[right]){
-                if(profit>max) 
+
+            if(prices[left] < prices[right]){
+                if(profit>max)
                     max = profit;
                 right++;
             }
@@ -18,7 +19,9 @@ class Solution {
                 left = right;
                 right++;
             }
-        }
+
+        }        
         return max;
     }
+
 }
